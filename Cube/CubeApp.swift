343e7eadc000088f6698fdcb19d6726f938d54ca
Alper Karatas <malperk@gmail.com>
2025-07-30 12:27:35 +0100

    Initial Commit

---
+++ Cube/CubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CubeApp.swift
+//  Cube
+//
+//  Created by Alper Karatas on 30/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct CubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
